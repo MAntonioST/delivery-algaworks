2a59162f83047f985e0d2d0fdfd9bce6f8265c00
Marco Antonio <marco.teixeira4@fatec.sp.gov.br>
2021-03-19 09:26:48 -0300

    Consulta simples com Criteria API

--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,15 +1,15 @@
 package com.algaworks.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQuery;
@@ -19,38 +19,16 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQuery {
 
 	@PersistenceContext
 	private EntityManager manager;
-	
+
 	@Override
-	public List<Restaurante> find(String nome,
-			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
-				
-		var jpql = new StringBuilder(); 
-		jpql.append("From Restaurante WHERE 0 = 0 ");
-		
-		var parametros = new HashMap<String,Object>();
-		
-		
-		if(StringUtils.hasLength(nome)) {
-			jpql.append("AND nome like :nome ");
-			parametros.put("nome", "%" + nome + "%");
-		}
-		
-		if(taxaFreteInicial != null) {
-			jpql.append("AND taxaFrete >= :taxaInicial ");
-			parametros.put("taxaInicial", taxaFreteInicial);
-		}
-		
-		if(taxaFreteFinal != null) {
-			jpql.append("AND taxaFrete <= :taxaFinal ");
-			parametros.put("taxaFinal", taxaFreteFinal);
-		}
-		
-		TypedQuery<Restaurante> query = manager
-				.createQuery(jpql.toString(), Restaurante.class);
-		
-		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
-		
+	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+
+		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+		criteria.from(Restaurante.class);
+
+		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		return query.getResultList();
-				
 	}
 }

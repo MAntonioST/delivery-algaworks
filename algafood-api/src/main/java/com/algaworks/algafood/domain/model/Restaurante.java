da1d781df294e0a55357050caaade156994f16bd
Marco Antonio <marco.teixeira4@fatec.sp.gov.br>
2021-03-20 11:44:25 -0300

    Mapeando classes incorporáveis com @Embedded e @Embeddable

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,11 +1,11 @@
 package com.algaworks.algafood.domain.model;
 
-import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,14 +15,15 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Restaurante implements Serializable {
-	private static final long serialVersionUID = 1L;
+public class Restaurante {
 
 	@EqualsAndHashCode.Include
 	@Id
@@ -36,13 +37,17 @@ public class Restaurante implements Serializable {
 	private BigDecimal taxaFrete;
 	
 	@ManyToOne
-	@JoinColumn(name ="cozinha_id", nullable = false)
+	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
 	
+	@Embedded
+	private Endereco endereco;
+	
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "restaurante_forma_pagamento",
-	         joinColumns = @JoinColumn(name = "restaurante_id"),
-	         inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
-	private List<FormaPagamento> formasPamamento = new ArrayList<>();
+			joinColumns = @JoinColumn(name = "restaurante_id"),
+			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	private List<FormaPagamento> formasPagamento = new ArrayList<>();
 	
 }
--- algafood-api/src/main/resources/import.sql
+++ algafood-api/src/main/resources/import.sql
@@ -1,3 +1,4 @@
+  
 insert into cozinha (id, nome) values (1, 'Tailandesa');
 insert into cozinha (id, nome) values (2, 'Indiana');
 
